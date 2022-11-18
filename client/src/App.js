78e625db415e789af55e009e25ccf2f317b81763
Sam Hilliard <samhilliard51@gmail.com>
2022-11-18 00:09:11 -0800

    navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import NavMenu from './Components/NavMenu/NavMenu';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavMenu />
+      <h1>Kyler Walsh</h1>
     </div>
   );
 }
---
+++ client/src/Components/NavMenu/NavMenu.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function NavMenu() {
+  return (
+    <div className="nav-menu">
+        <MenuIcon />
+    </div>
+  )
+}
