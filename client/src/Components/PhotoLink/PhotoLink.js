2b73b8f351cbde55fe684f80642a893e95bfd6ae
Sam Hilliard <samhilliard51@gmail.com>
2022-11-19 23:42:13 -0800

    styled landing page

--- client/src/Components/PhotoLink/PhotoLink.js
+++ client/src/Components/PhotoLink/PhotoLink.js
@@ -6,7 +6,7 @@ import styles from './PhotoLink.module.css'
 
 export default function PhotoLink(props) {
   return (
-    <div className={styles.photo_link}>
+    <div className={styles.container}>
         <img className={styles.bg_img} alt={props.title} src={props.img} />
         <Link className={styles.link_title} to={props.link}>{props.title}</Link>
     </div>
--- client/src/Pages/LandingPage/LandingPage.js
+++ client/src/Pages/LandingPage/LandingPage.js
@@ -5,20 +5,29 @@ import PhotoLink from '../../Components/PhotoLink/PhotoLink'
 import photoSample from '../../assets/images/photo-sample.jpg'
 import artSample from '../../assets/images/art-sample.jpg'
 
+import styles from './LandingPage.module.css'
+
 export default function LandingPage() {
   return (
-    <div className="landing-page">
-      <h1>Kyler Walsh</h1>
-      <PhotoLink 
-        img={photoSample}
-        link="" 
-        title="Photographer"
-      />
-      <PhotoLink 
-        img={artSample} 
-        link="" 
-        title="Artist"
-      />
+    <div className={styles.container}>
+      <h1>
+        Kyler
+        <br />
+        Walsh
+      </h1>
+      <div className="separater"></div>
+      <div className={styles.photo_container}>
+        <PhotoLink 
+          img={photoSample}
+          link="" 
+          title="Photographer"
+        />
+        <PhotoLink 
+          img={artSample} 
+          link="" 
+          title="Artist"
+        />
+      </div>
     </div>
   )
 }
