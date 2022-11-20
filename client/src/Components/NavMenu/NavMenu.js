fbdfc815ef98aa3b1629b94bbaecc0b2b7faa4f9
Sam Hilliard <samhilliard51@gmail.com>
2022-11-19 23:43:12 -0800

    resized menu icon and added module stylesheet

--- client/src/Components/NavMenu/NavMenu.js
+++ client/src/Components/NavMenu/NavMenu.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import MenuIcon from '@mui/icons-material/Menu';
 
+import styles from './NavMenu.module.css';
+
 export default function NavMenu() {
   return (
     <div className="nav-menu">
-        <MenuIcon />
+        <MenuIcon className={styles.menu_icon} />
     </div>
   )
 }
