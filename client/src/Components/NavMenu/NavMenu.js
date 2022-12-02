f5e9354ad6005e551088b6c1a65457cba55cb5f0
Sam Hilliard <samhilliard51@gmail.com>
2022-12-01 18:40:34 -0800

    responsively hide header underline on landing page

--- client/src/Components/NavMenu/NavMenu.js
+++ client/src/Components/NavMenu/NavMenu.js
@@ -10,6 +10,10 @@ export default function NavMenu() {
 
   const navItems = [
     {
+      name: 'Home',
+      link: '/'
+    },
+    {
       name: 'About',
       link: '/about'
     },
--- client/src/Pages/LandingPage/LandingPage.js
+++ client/src/Pages/LandingPage/LandingPage.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import Header from '../../Components/Header/Header'
-
 import styles from './LandingPage.module.css'
 
 export default function LandingPage() {
   return (
     <div className={styles.container}>
-        <div className={styles.header}><Header title={<>Kyler<br />Walsh</>} /></div>
+        <div className={styles.header}>
+          <div className="centered">
+            <h1>Kyler<br />Walsh</h1>
+            <div className={`separater ${styles.underline}`}></div>
+          </div>
+        </div>
         <div className={`bg-img-fill ${styles.photo_sample}`} />
         <Link className={`title-link ${styles.photo_link}`}>Photographer</Link>
         <div className={`bg-img-fill ${styles.art_sample}`} />
