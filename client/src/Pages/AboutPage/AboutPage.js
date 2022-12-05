18676bf5762bbacc0eae1ee9e57f1fe0d73ace7f
Sam Hilliard <samhilliard51@gmail.com>
2022-12-05 14:15:33 -0800

    added content to about page and resized p font-size

--- client/src/Pages/AboutPage/AboutPage.js
+++ client/src/Pages/AboutPage/AboutPage.js
@@ -2,10 +2,17 @@ import React from 'react'
 
 import Header from '../../Components/Header/Header'
 
+import aboutInfo from '../../assets/content/aboutme.json'
+import profilePic from '../../assets/images/profile-pic.jpg'
+
 export default function AboutPage() {
   return (
     <div className="container">
         <Header title="About" />
+        <div>
+          <div><img src={profilePic} alt="What Kyler looks like." /></div>
+          <p>{aboutInfo.about}</p>
+        </div>
     </div>
   )
 }
