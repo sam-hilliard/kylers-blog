2b32c4a8d06de32f70c2376ec63a2722d5cd9f7b
Sam Hilliard <samhilliard51@gmail.com>
2022-12-05 15:28:49 -0800

    split layout for about page

--- client/src/Pages/AboutPage/AboutPage.js
+++ client/src/Pages/AboutPage/AboutPage.js
@@ -5,12 +5,14 @@ import Header from '../../Components/Header/Header'
 import aboutInfo from '../../assets/content/aboutme.json'
 import profilePic from '../../assets/images/profile-pic.jpg'
 
+import styles from './AboutPage.module.css'
+
 export default function AboutPage() {
   return (
     <div className="container">
         <Header title="About" />
-        <div>
-          <div><img src={profilePic} alt="What Kyler looks like." /></div>
+        <div className="split">
+          <div className={`centered ${styles.imgContainer}`}><img src={profilePic} alt="What Kyler looks like." /></div>
           <p>{aboutInfo.about}</p>
         </div>
     </div>
